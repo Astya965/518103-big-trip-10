439552d20439797f4720f12385ceb78fdbecb3a2
astya965 <astya965@gmail.com>
2019-12-26 16:02:50 +0300

    Add: function for rendering in util.js

--- src/util.js
+++ src/util.js
@@ -13,13 +13,18 @@ export const MonthNames = [
   `DEС`
 ];
 
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
 /**
  * Выбор случайного числа в заданном промежутке
  * @param {Number} min - Минимальное допустимое значение (включительно)
  * @param {Number} max - Максимальное допустимое значение (включительно)
  * @return {Number} Случайное целое число
  */
-const getRandomNumber = (min, max) => {
+export const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
@@ -28,7 +33,7 @@ const getRandomNumber = (min, max) => {
  * @param {Array} array
  * @return {Any} Случайный элемент массива
  */
-const getRandomElement = (array) => {
+export const getRandomElement = (array) => {
   const randomIndex = Math.floor(Math.random() * (array.length));
   return array[randomIndex];
 };
@@ -38,7 +43,7 @@ const getRandomElement = (array) => {
  * @param {Array} array - Массив, который нужно перемешать
  * @return {Array} Перемешенный массив
  */
-const mixArray = function (array) {
+export const mixArray = function (array) {
   for (let i = array.length - 1; i > 0; i--) {
     let k = Math.floor(Math.random() * (i + 1));
     let swap = array[k];
@@ -53,9 +58,38 @@ const mixArray = function (array) {
  * Генерация случайного значения true\false
  * @return {Boolean}
  */
-const getRandomBoolean = () => {
+export const getRandomBoolean = () => {
   return Math.floor(Math.random() < 0.5);
 };
 
-export {getRandomNumber, getRandomElement, getRandomBoolean, mixArray};
+/**
+ * Создание элемента разметки
+ * @param {String} template - Разметка в виде строки
+ * @return {HTMLElement} - Элемент разметки
+ */
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
 
+  return newElement.firstChild;
+};
+
+/**
+ * Рендеринг элемента разментки
+ * @param {HTMLElement} container - Элемент разметки, куда добавляется новый элемент
+ * @param {HTMLElement} element - Новый элемент, добавляемый в DOM-дерево
+ * @param {DOMString} place - Определяет позицию добавляемого элемента относительно элемента, вызвавшего метод
+ */
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
